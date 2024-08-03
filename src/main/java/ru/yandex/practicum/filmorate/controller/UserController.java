f06d01cd46ff4f94577bf3d58bffaf9884ac5113
MariaB <peggy-sju@ya.ru>
2024-08-03 20:44:31 +0300

    Add validation in UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -6,6 +6,7 @@ import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,6 +23,26 @@ public class UserController {
 
     @PostMapping
     public User create(@RequestBody User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ConditionsNotMetException("Электронная почта не может быть пустой");
+        }
+        if (!user.getEmail().contains("@")) {
+            throw new ConditionsNotMetException("Электронная почта должна содержать символ @");
+        }
+        if (user.getLogin() == null || user.getLogin().isBlank()) {
+            throw new ConditionsNotMetException("Логин не может быть пустым");
+        }
+        if (user.getLogin().contains(" ")) {
+            throw new ConditionsNotMetException("Логин не может содержать пробелы");
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ConditionsNotMetException("Дата рождения не может быть в будущем.");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        } else {
+            user.setName(user.getName());
+        }
         user.setId(getNextId());
         users.put(user.getId(), user);
         return user;
@@ -34,9 +55,28 @@ public class UserController {
         }
         if (users.containsKey(newUser.getId())) {
             User oldUser = users.get(newUser.getId());
+            if (newUser.getEmail() == null || newUser.getEmail().isBlank()) {
+                throw new ConditionsNotMetException("Электронная почта не может быть пустой");
+            }
+            if (!newUser.getEmail().contains("@")) {
+                throw new ConditionsNotMetException("Электронная почта должна содержать символ @");
+            }
+            if (newUser.getLogin() == null || newUser.getLogin().isBlank()) {
+                throw new ConditionsNotMetException("Логин не может быть пустым");
+            }
+            if (newUser.getLogin().contains(" ")) {
+                throw new ConditionsNotMetException("Логин не может содержать пробелы");
+            }
+            if (newUser.getBirthday().isAfter(LocalDate.now())) {
+                throw new ConditionsNotMetException("Дата рождения не может быть в будущем.");
+            }
             oldUser.setEmail(newUser.getEmail());
             oldUser.setLogin(newUser.getLogin());
-            oldUser.setName(newUser.getName());
+            if (newUser.getName() == null || newUser.getName().isBlank()) {
+                oldUser.setName(newUser.getLogin());
+            } else {
+                oldUser.setName(newUser.getName());
+            }
             oldUser.setBirthday(newUser.getBirthday());
             return oldUser;
         }
