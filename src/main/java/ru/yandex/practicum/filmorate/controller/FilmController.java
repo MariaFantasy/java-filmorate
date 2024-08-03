163c7bb578153f5ddec0c516cc8ae4e057b55f34
MariaB <peggy-sju@ya.ru>
2024-08-03 20:37:40 +0300

    Add validation in FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -6,6 +6,7 @@ import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,6 +23,18 @@ public class FilmController {
 
     @PostMapping
     public Film create(@RequestBody Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ConditionsNotMetException("Название не может быть пустым");
+        }
+        if (film.getDescription().length() > 200) {
+            throw new ConditionsNotMetException("Максимальная длина описания — 200 символов");
+        }
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ConditionsNotMetException("Дата релиза — не раньше 28 декабря 1895 года");
+        }
+        if (film.getDuration().isNegative()) {
+            throw new ConditionsNotMetException("Продолжительность фильма должна быть положительным числом");
+        }
         film.setId(getNextId());
         films.put(film.getId(), film);
         return film;
@@ -34,6 +47,18 @@ public class FilmController {
         }
         if (films.containsKey(newFilm.getId())) {
             Film oldFilm = films.get(newFilm.getId());
+            if (newFilm.getName() == null || newFilm.getName().isBlank()) {
+                throw new ConditionsNotMetException("Название не может быть пустым");
+            }
+            if (newFilm.getDescription().length() > 200) {
+                throw new ConditionsNotMetException("Максимальная длина описания — 200 символов");
+            }
+            if (newFilm.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+                throw new ConditionsNotMetException("Дата релиза — не раньше 28 декабря 1895 года");
+            }
+            if (newFilm.getDuration().isNegative()) {
+                throw new ConditionsNotMetException("Продолжительность фильма должна быть положительным числом");
+            }
             oldFilm.setName(newFilm.getName());
             oldFilm.setDescription(newFilm.getDescription());
             oldFilm.setReleaseDate(newFilm.getReleaseDate());
