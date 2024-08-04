bbb15dfff329a54f47958299325313fbcd955c7f
MariaB <peggy-sju@ya.ru>
2024-08-05 01:07:00 +0300

    Change logs from FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -23,33 +23,37 @@ public class FilmController {
 
     @GetMapping
     public Collection<Film> findAll() {
+        log.info("Пришел GET запрос /films");
+        log.info("Отправлен ответ GET /films с телом: " + films.values());
         return films.values();
     }
 
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
+        log.info("Пришел POST запрос /films с телом: " + film);
         validate(film);
         final long filmId = getNextId();
         film.setId(filmId);
         films.put(filmId, film);
-        log.info("Film with id " + filmId  + " created");
+        log.info("Отправлен ответ POST /films с телом: " + film);
         return film;
     }
 
     @PutMapping
     public Film update(@Valid @RequestBody Film film) {
+        log.info("Пришел PUT запрос /films с телом: " + film);
         final Long filmId = film.getId();
         if (filmId == null) {
-            log.debug("Film has not been updated: Id is empty");
+            log.info("Запрос PUT /films обработан не был по причине: Id должен быть указан");
             throw new ConditionsNotMetException("Id должен быть указан");
         }
         if (films.containsKey(filmId)) {
             validate(film);
             films.put(filmId, film);
-            log.info("Film with id " + filmId  + " updated");
+            log.info("Отправлен ответ PUT /films с телом: " + film);
             return film;
         }
-        log.info("Film has not been updated: Id not found");
+        log.info("Запрос PUT /films обработан не был по причине: Фильм с id = " + filmId + " не найден");
         throw new NotFoundException("Фильм с id = " + filmId + " не найден");
     }
 
@@ -59,11 +63,11 @@ public class FilmController {
 
     private void validate(final Film film) {
         if (film.getDescription().length() > 200) {
-            log.debug("Film has not been created: Description too long");
+            log.info("Фильм не прошел валидацию по причине: Максимальная длина описания — 200 символов");
             throw new ConditionsNotMetException("Максимальная длина описания — 200 символов");
         }
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            log.debug("Film has not been created: Release Date is too early");
+            log.info("Фильм не прошел валидацию по причине: Дата релиза — не раньше 28 декабря 1895 года");
             throw new ConditionsNotMetException("Дата релиза — не раньше 28 декабря 1895 года");
         }
     }
