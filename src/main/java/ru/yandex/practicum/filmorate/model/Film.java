29cb8feade69959a7c791feaf9d141169f227f0d
MariaB <peggy-sju@ya.ru>
2024-09-11 20:33:00 +0300

    Доработка в связи с ревью 1 - Добавление JsonIgnore в Film и User для запрета взаимодействия с полями не через эндпоинты

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -26,5 +27,6 @@ public class Film {
     @Positive(message = "Продолжительность фильма должна быть положительным числом")
     private int duration;
 
+    @JsonIgnore
     private final Set<Long> likedUsers = new HashSet<>();
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -27,5 +28,6 @@ public class User {
     @NotNull(message = "Дата рождения пользователя не может быть пустой")
     private LocalDate birthday;
 
+    @JsonIgnore
     private final Set<Long> friends = new HashSet<>();
 }
