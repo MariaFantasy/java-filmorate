72e7517ea9f200d4d19ba84c5c16a782980707da
MariaFantasy <peggy-sju@ya.ru>
2024-11-17 07:20:29 +0300

    Создание базы данных - Создание скриптов с инициализацией базы данных

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+INSERT INTO genre (name)
+VALUES
+    ("COMEDY"),
+    ("DRAMA"),
+    ("CARTOON"),
+    ("THRILLER"),
+    ("DOCUMENTARY"),
+    ("ACTION");
+
+INSERT INTO rating (name)
+VALUES
+    ("G"),
+    ("PG"),
+    ("PG-13"),
+    ("R"),
+    ("NC-17");
+
+INSERT INTO friendship_status (name)
+VALUES
+ ("Accept"),
+ ("Waiting Accept");
