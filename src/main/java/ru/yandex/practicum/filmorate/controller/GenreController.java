ae346f89d0ac6afe6f6af3f0b4dbc611e3220a48
MariaFantasy <peggy-sju@ya.ru>
2024-11-24 22:58:48 +0300

    Корректировка в связи с ревью - Удаление из класса контроллера жанров случая, когда genre == null

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.service.GenreService;
 
@@ -32,10 +31,6 @@ public class GenreController {
     public Genre findById(@PathVariable Integer genreId) {
         log.info("Пришел GET запрос /genres/{}", genreId);
         final Genre genre = genreService.findById(genreId);
-        if (genre == null) {
-            log.info("Запрос GET /genres/{} обработан не был по причине: Жанр c id = {} не найден", genreId, genreId);
-            throw new NotFoundException("Жанр с id = " + genreId + " не найден.");
-        }
         log.info("Отправлен ответ GET /genres/{} с телом: {}", genreId, genre);
         return genre;
     }
