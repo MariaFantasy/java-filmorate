14fa716aba81eba77faf88d99ba73cc252fe96fa
MariaFantasy <peggy-sju@ya.ru>
2024-11-24 22:59:07 +0300

    Корректировка в связи с ревью - Удаление из класса контроллера рейтингов случая, когда rating == null

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.service.MpaService;
 
@@ -32,10 +31,6 @@ public class MpaController {
     public Mpa findById(@PathVariable Integer mpaId) {
         log.info("Пришел GET запрос /mpa/{}", mpaId);
         final Mpa mpa = mpaService.findById(mpaId);
-        if (mpa == null) {
-            log.info("Запрос GET /mpa/{} обработан не был по причине: Рейтинг с id = {} не найден", mpaId, mpaId);
-            throw new NotFoundException("Рейтинг с id = " + mpaId + " не найден.");
-        }
         log.info("Отправлен ответ GET /mpa/{} с телом: {}", mpaId, mpa);
         return mpa;
     }
