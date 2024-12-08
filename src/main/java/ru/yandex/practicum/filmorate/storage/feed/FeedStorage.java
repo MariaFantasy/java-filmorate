aea154bc7cec2619920a7bff0c5509c13d4c49a0
MariaFantasy <peggy-sju@ya.ru>
2024-12-08 08:14:19 +0300

    Реализация нового функционала - Добавление интерфейса и классов Storage для обработки ленты событий

--- src/main/java/ru/yandex/practicum/filmorate/storage/feed/FeedStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/feed/FeedStorage.java
@@ -1,4 +1,13 @@
 package ru.yandex.practicum.filmorate.storage.feed;
 
-public class FeedStorage {
+import ru.yandex.practicum.filmorate.model.Event;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+
+public interface FeedStorage {
+
+    public Event create(Event event);
+
+    public Collection<Event> getUserFeed(User user);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/feed/InMemoryFeedStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/feed/InMemoryFeedStorage.java
@@ -1,4 +1,35 @@
 package ru.yandex.practicum.filmorate.storage.feed;
 
-public class InMemoryFeedStorage {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Event;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component("inMemoryFeedStorage")
+@RequiredArgsConstructor
+public class InMemoryFeedStorage implements FeedStorage {
+    private final Map<Long, Event> events = new HashMap<>();
+    private long eventCounter = 0;
+
+    @Override
+    public Event create(Event event) {
+        final long eventId = getNextId();
+        event.setEventId(eventId);
+        events.put(eventId, event);
+        return event;
+    }
+
+    @Override
+    public Collection<Event> getUserFeed(User user) {
+        return events.values().stream()
+                .filter(event -> Objects.equals(event.getUserId(), user.getId()))
+                .collect(Collectors.toCollection(HashSet::new));
+    }
+
+    private long getNextId() {
+        return ++eventCounter;
+    }
 }
